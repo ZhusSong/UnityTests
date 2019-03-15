a0b19dae9fc9877e6c37d8393b9f74b46ef10e22
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-03-15 15:13:02 +0800

    19.3.15.15.12

---
+++ 2D test/Assets/PenguinAdventure/Script/Monsters/MonsterBlock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 检测怪物是否被唤醒，使用地图中已预制的碰撞盒进行检测。
+/// </summary>
+public class MonsterBlock : MonoBehaviour {
+	public GameObject BlockMonster;
+	public GameObject BlockEyes;
+	public bool isSurprise = false;
+	public AudioClip soundShowUp;
+
+	void Awake(){
+		BlockMonster.SetActive (false);
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			if (isSurprise)
+				SoundManager.PlaySfx (soundShowUp);
+			BlockMonster.SetActive (true);
+			BlockEyes.SetActive (false);
+			GetComponent<BoxCollider2D> ().enabled = false;
+			enabled = false;
+		}
+	}
+}
---
+++ 2D test/Assets/PenguinAdventure/Script/Other/DestroyFalling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 坠落碰撞体，位于场景下方，当接触到玩家时控制玩家死亡
+/// </summary>
+public class DestroyFalling : MonoBehaviour {
+	public AudioClip soundWater;
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if (other.gameObject.CompareTag ("Player")) {
+			Debug.Log ("GAMEOVER");
+			SoundManager.PlaySfx (soundWater);
+			GameManager.instance.GameOver ();
+			other.gameObject.SetActive (false);
+		} else
+			Destroy (other.gameObject);
+	}
+}
