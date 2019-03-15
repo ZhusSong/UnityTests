498c9ffa0396ffdc085e81951ac9d183c10a5d57
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-03-15 14:55:24 +0800

    19.3.15.14.55

--- CardsAndDices/Assets/Scripts/Map/AllMonsters.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-//存储怪物图片、动画以及技能的库
-public class AllMonsters : MonoBehaviour {
-    public GameObject[] MonsterCards=new GameObject[5];
-    public GameObject[] Monsters=new GameObject[5];
-	
-}
